eea47d4080ccdd1104d07394e96298d2b488e9b0
amd1217@126.com <amd1217@126.com>
2019-05-26 23:29:04 +0800

    add stm32mp15x project symbol files

---
+++ allegro clean.bat
@@ -0,0 +1,10 @@
+cd D:\myshare\hw-lib\Cadence_lib
+del /a /f /s *.log
+del /a /f /s *.tag
+del /a /f /s *f*.opj
+del /a /f /s *.jrl*
+del /a /f /s signosie*
+del /a /f /s pstchip*
+del /a /f /s pstxnet*
+del /a /f /s pstxprt*
+del /a /f /s *.*log*
